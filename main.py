1ac9d154bbb90c5ea14813dabb8688a50208d648
VENOM-CRAZY-OP <cyberecurityind@gmail.com>
2024-10-14 16:38:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def install_requirements():
 install_requirements()
 
 # 🎛️ Telegram API token (replace with your actual token)
-TOKEN = '7424479555:AAGtNAbqXsHax_b3b--QunnGYi5_XwBbQNw'
+TOKEN = '7424479555:AAGtNAbqXax_b3b--QnGYi5_XwBbQNw'
 bot = telebot.TeleBot(TOKEN, threaded=False)
 
 # 🛡️ List of authorized user IDs (replace with actual IDs)
